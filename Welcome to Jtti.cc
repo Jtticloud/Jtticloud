4325ae19433ba8d1457dd7046c021c56b4f083e6
jtti <market@jtti.cc>
2024-06-14 14:51:42 +0800

    Create Welcome to Jtti.cc
    Cloud Server
    Cloud servers are designed with high availability and fully redundant architecture, giving you the flexibility to scale as your business grows.
    
    Server Hosting
    We provide server hosting services from our finely tuned premium data center in a prime location. We have achieved 99.98% uptime for many years.
    
    Security Service
    We also offer additional TLDs and SSL certificates to prepare your business for future expansion while extensions are still available.

---
+++ Welcome to Jtti.cc
@@ -0,0 +1,16 @@
+
+Jtti, short for JT TELECOM INTERNATIONAL PTE.LTD, is a global network infrastructure service provider incorporated in Singapore,
+mainly provides high-quality one-stop Internet service solutions, including cloud servers, 
+server leasing, server hosting, security technology development and other products and services.
+
+Senior Elite，Industry leader Jtti teams all come from the world's leading network service providers,
+80% of the members have more than 10 years of industry experience,
+with professional R & D capabilities and operation and maintenance technology, with excellent infrastructure,
+network technology, and years of customer service experience. 
+So far Jtti has provided Internet infrastructure services and network hosting services to a total of tens of thousands of enterprises around the world, 
+which has made Jtti the world's leading enterprise network infrastructure services provider of Asia and the whole world.
+
+Today, Jtti has established its own global backbone network,
+while deploying multiple data centers in the Americas and Asia with over 5,000 dedicated servers. 
+In the future, Jtti will continue to actively expand into the European market, accelerate data center deployments, 
+and further expand its partnerships with top global network service providers to continue to provide high-speed networks and quality services to its customers.
